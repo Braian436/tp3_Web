fa7dde1489324c606c95dd6d5dd63e5b1dbc27de
Braian <braianalexisamaya16@gmail.com>
2026-09-15 18:40:29 -0300

    Añadi Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 import styles from './App.module.css';
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <div className={styles.appContainer}>
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 }
